6498cca1190638b8a5dc9e5aac6a31e29053f623
nihansulukoyozen <nihansulukoy@gmail.com>
2024-06-25 17:29:52 +0300

    created update book method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+
 public class Main {
     static int Index = 50;
     static String[][] books = new String[Index][4]; // title , author , id , additional doc.
     static String[][] users = new String[Index][4];
-    static String[][] transactions = new String[Index][3];
+    static String[][] transactions = new String[Index][4]; // userId , bookId , date , status
     static int bookQuantity = 0;
     static int transactionQuantity = 0;
     static int userQuantity = 0;
@@ -11,8 +12,8 @@ public class Main {
     public static void main(String[] args) {
     }
 
-    public static void addBook(String title, String author, String bookId, String additionalDoc){
-        if(bookQuantity < Index){
+    public static void addBook(String title, String author, String bookId, String additionalDoc) {
+        if (bookQuantity < Index) {
             books[bookQuantity][0] = title;
             books[bookQuantity][1] = author;
             books[bookQuantity][2] = bookId;
@@ -20,14 +21,14 @@ public class Main {
             bookQuantity++;
 
             System.out.println(bookQuantity + "." + " Book added successfully");
-        }else {
+        } else {
             System.out.println("We cannot add books to shelves that are already full!");
 
         }
     }
 
     public static void viewAvailableBooks() {
-        
+
         if (bookQuantity == 0) {
             System.out.println("There isn't available books!");
         } else {
@@ -44,19 +45,149 @@ public class Main {
         System.out.println("Book ID: " + bookId);
         System.out.println("Book Additional Document: " + additionalDoc);
     }
-   
+
     //Total number of books
     public static void countTotalBooks() {
         System.out.println("Total number of books :" + bookQuantity);
     }
-    static void checkBooks(String bookId){  
+
+    static void checkBooks(String bookId) {
         boolean found = false;
         for (int i = 0; i < bookQuantity; i++) {
-                if (books[i][2].equals(bookId)) {
-                    System.out.println("The book is found!");
-                    found = true;
-                break;}}
-    if (!found) {
-        System.out.println("The book is not found.");
+            if (books[i][2].equals(bookId)) {
+                System.out.println("The book is found!");
+                found = true;
+                break;
+            }
         }
-}}
+        if (!found) {
+            System.out.println("The book is not found.");
+        }
+    }
+
+    public static void searchBook(String query) { // Search with bookTitle or bookId
+        int temp = -1, i;
+        for (i = 0; i < bookQuantity; i++) {
+            if (books[i][0].equals(query) || books[i][2].equals(query)) {
+                System.out.println("The book was found :");
+                System.out.println("--Book Information--");
+                System.out.println("Book title : " + books[i][0]);
+                System.out.println("Book Author : " + books[i][1]);
+                System.out.println("Book ID : " + books[i][2]);
+                System.out.println("Book Additional Document : " + books[i][3]);
+                temp = i;
+            }
+        }
+        if (temp == -1) {
+            System.out.println("The book is not found : ");
+        }
+    }
+
+    public static void returnbook(String userId, String bookId) {
+        int temp = -1, i;
+        for (i = 0; i < transactionQuantity; i++) {
+            if (transactions[i][0].equals(userId) || transactions[i][1].equals(bookId)) {
+                transactions[i][3] = "RETURNED";
+                System.out.println("Book return transaction successful");
+                temp = i;
+            }
+        }
+        if (temp == -1) {
+            System.out.println("Book return transaction failed!");
+        }
+    }
+
+    public static void updateBook(String title, String author, String bookId, String additionalDoc) {
+        int temp = -1, i;
+        for (i = 0; i < bookQuantity; i++) {
+            if (books[i][2].equals(bookId)) {
+                books[i][0] = title;
+                books[i][1] = author;
+                books[i][2] = bookId;
+                books[i][3] = additionalDoc;
+                System.out.println("Book update transaction successful");
+                temp = i;
+            }
+        }
+        if (temp == -1) {
+            System.out.println("Book update transaction failed!");
+        }
+    }
+
+    public static void deleteBook(String title, String author, String bookId, String additionalDoc) {
+        int temp = -1, i;
+        for (i = 0; i < bookQuantity; i++) {
+            if (books[i][0].equals(title) || books[i][2].equals(bookId)) {
+                books[i][0] = "";
+                books[i][1] = "";
+                books[i][2] = "";
+                books[i][3] = "";
+                System.out.println("Book delete transaction successful");
+                temp = i;
+            }
+            truncateBooksArrayOnDeletion(bookId);
+        }
+        if (temp == -1) {
+            System.out.println("Book delete transaction failed!");
+        }
+
+    }
+
+    public static void truncateBooksArrayOnDeletion(String bookId) {
+        int temp = -1, i, j;
+        for (i = 0; i < bookQuantity; i++) {
+            if (books[i][2].equals(bookId)) {
+                temp = i;
+                bookQuantity = bookQuantity - 1;
+                String[][] booksNew = new String[bookQuantity][4];
+                for (j = 0; j < temp; j++) {
+                    booksNew[j][0] = books[i][0];
+                    booksNew[j][1] = books[i][1];
+                    booksNew[j][2] = books[i][2];
+                    booksNew[j][3] = books[i][3];
+                }
+                for (j = temp; j < bookQuantity; j++) {
+                    booksNew[j][0] = books[i + 1][0];
+                    booksNew[j][1] = books[i + 1][1];
+                    booksNew[j][2] = books[i + 1][2];
+                    booksNew[j][3] = books[i + 1][3];
+                }
+                books = booksNew;
+                System.out.println("Truncate Books and Array On Deletion transaction successful");
+            }
+        }
+        if (temp == -1) {
+            System.out.println("Truncate Books and Array On Deletion transaction failed!");
+        }
+
+    }
+
+    public static void extendBooksArrayOnAddition(String title, String author, String bookId, String additionalDoc) {
+        int temp = -1, i, j;
+        if (bookQuantity < Index) {
+            for (i = 0; i < bookQuantity; i++) {
+                if (books[i][2].equals(bookId)) {
+                    bookQuantity = bookQuantity + 1;
+                    String[][] booksNew = new String[bookQuantity][4];
+                    for (j = 0; j < bookQuantity - 1; i++) {
+                        booksNew[j][0] = books[i][0];
+                        booksNew[j][1] = books[i][1];
+                        booksNew[j][2] = books[i][2];
+                        booksNew[j][3] = books[i][3];
+                    }
+
+                    booksNew[bookQuantity - 1][0] = title;
+                    booksNew[bookQuantity - 1][1] = author;
+                    booksNew[bookQuantity - 1][2] = bookId;
+                    booksNew[bookQuantity - 1][3] = additionalDoc;
+
+                    books = booksNew;
+                    System.out.println("Extend Books and Array On Addition transaction successful");
+                }
+            }
+            if (temp == -1) {
+                System.out.println("Extend Books and Array On Addition transaction failed!");
+            }
+        }
+    }
+}
